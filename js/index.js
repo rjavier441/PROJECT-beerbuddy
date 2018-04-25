b088217f5d37c4fd0688d050ad4f8a2ebc2a570d
Rolando Javier <rjavier441@gmail.com>
2018-04-24 23:11:49 -0700

    This commit adds the initial homepage for the beer buddy web app (index.html) and corresponding javascript (js/index.js).

---
+++ js/index.js
@@ -0,0 +1,25 @@
+//	PROJECT: 		BeerBuddy
+// 	Name: 			Rolando Javier
+// 	File: 			index.js
+// 	Date Created: 	April 24, 2018
+// 	Last Modified: 	April 24, 2018
+// 	Details:
+// 					This file holds the javascript that controls the index.html homepage
+// 	Dependencies:
+// 					JQuery 3.x.x or higher
+//					Bootstrap 3.x.x
+
+$(document).ready(init);
+
+/*
+	@function 	init
+	@parameter 	n/a
+	@returns 	n/a
+	@details 	This function initializes the page
+*/
+function init () {
+	// Initialize page here...
+	console.log("BeerBuddy: Your buddy in beer!");
+}
+
+// END index.js
