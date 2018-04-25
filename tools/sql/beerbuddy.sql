e469fad349328613b7c79eca98ec1f11611f0e56
Rolando Javier <rjavier441@gmail.com>
2018-04-24 20:02:26 -0700

    Initial commit that adds the completed DDL script to initiate the DB

---
+++ tools/sql/beerbuddy.sql
@@ -0,0 +1,132 @@
+drop database if exists beerbuddy;
+create database beerbuddy;
+use beerbuddy;
+
+
+CREATE TABLE user(
+	username varchar(30) NOT NULL,
+    birthdate datetime,
+    PRIMARY KEY (username),
+    unique (username)
+);
+
+create table bar_owner
+(
+	username		varchar(30) NOT NULL,
+	owner_name		varchar(50) NOT NULL,
+    unique (username),
+	foreign key (username) references user(username)
+);
+
+create table bar
+(
+    username		varchar(30) NOT NULL, 
+	bar_name		varchar(20) NOT NULL, 
+	bar_id INT(5) NOT NULL,
+	primary key (bar_id,username),
+	foreign key (username) references bar_owner(username)
+);
+
+CREATE TABLE bar_location 
+(
+	cityname varchar(25) not null,
+    state_name varchar(25) not null,
+    address varchar(25) not null,
+	bar_id INT(5) NOT NULL,
+    unique (bar_id),
+    foreign key (bar_id) references bar(bar_id)
+);
+
+CREATE TABLE drink
+(
+	name varchar(50) NOT NULL,
+	alcohol_content int(10),
+	type varchar(20),
+	calories int(10),
+	price int(10),
+	bar_id INT(5) NOT NULL,
+    primary key (bar_id),
+    unique (name),
+    foreign key (bar_id) references bar(bar_id)
+);
+
+-- Ingredients
+CREATE TABLE ingredients (
+	name VARCHAR(50) NOT NULL, 
+	bar_id INT(5) NOT NULL,
+	ing_name VARCHAR(30) NOT NULL,
+    unique (name),
+    foreign key (bar_id) references drink(bar_id),
+    foreign key (name) references drink(name)
+    );
+
+-- ALTER TABLE ingredients ADD CONSTRAINT FK_ingredients_name 
+-- 	FOREIGN KEY (name) REFERENCES drink(name);
+    
+-- ALTER TABLE ingredients ADD CONSTRAINT FK_ingredients_bar_id 
+-- 	FOREIGN KEY (bar_id) REFERENCES drink(bar_id);
+
+-- Serves
+CREATE TABLE serves  
+(
+	username VARCHAR(20) NOT NULL, 
+	bar_id INT(5) NOT NULL,
+	name VARCHAR(30) NOT NULL,
+    primary key (username, bar_id),
+    foreign key (username) references bar(username),
+    foreign key (bar_id) references bar(bar_id)
+);
+
+-- ALTER TABLE serves ADD CONSTRAINT FK_serves_username FOREIGN 
+-- 	KEY (username) REFERENCES bar(username);
+
+-- ALTER TABLE serves ADD CONSTRAINT FK_serves_bar_id FOREIGN KEY 
+-- 	(bar_id) REFERENCES bar(bar_id);
+    
+CREATE TABLE in_house(
+	name varchar(50) NOT NULL,
+	in_house_taste varchar(50),
+	bar_id INT(5) NOT NULL,
+	primary key (bar_id),
+    unique key (name),
+    FOREIGN KEY (bar_id) REFERENCES serves(bar_id),
+    foreign key (name) references drink(name)
+);
+
+CREATE TABLE common_brand(
+	name varchar(50) NOT NULL REFERENCES drink(name),
+	manufacture varchar(50),
+	common_taste varchar(50),
+	bar_id INT(5) NOT NULL,
+	PRIMARY KEY(bar_id),
+    unique (name),
+	FOREIGN KEY (bar_id) REFERENCES serves(bar_id),
+    foreign key (name) references drink(name)
+);
+    
+CREATE TABLE rate_drink 
+(
+	username varchar(25) not null,
+    name varchar(25) not null,
+    stars integer,
+    taste varchar(25),
+	bar_id INT(5) NOT NULL,
+    primary key (username),
+    unique (username),
+    unique (bar_id),
+    foreign key (username) references user(username),
+    foreign key (bar_id) references serves(bar_id)
+);
+
+CREATE TABLE rate_bar (
+	username varchar(30) NOT NULL,
+	bar_id INT(5) NOT NULL,
+    stars integer NOT NULL,
+    PRIMARY KEY (username, bar_id),
+    foreign key (username) references user(username),
+    foreign key (bar_id) references bar(bar_id)
+);
+
+-- ALTER TABLE drink ADD FOREIGN KEY (bar_id) REFERENCES serves(bar_id);
+
+
