dc1b5313386f2407ad49db41ed6a7329265f9021
Varinder Singh <1995.varindersingh@gmail.com>
2018-05-03 06:02:09 +0000

    Merged in vs/rateDrinknBar (pull request #9)
    Vs/rateDrinknBar
    
    * It is creating a rating system for Drinks. This commit add a very basic
        html file.
    
    * It is creating a rating system for Drinks. This commit add a very basic
        html file.
    
    * modified the rateDrinknBar/rateDrinknBar:q.html file and created the
    
    * Edited the rateDrinknBar.js and rateDrinknBar.html and connected them var searchDrink = $("#searchDrink").val();
            var describeDrink = $("#describeDrink").val();
            var rateDrink = $("#rateDrink").val();
            var searchBar = $("#searchBar").val();
            var rateBar = $("#rateBar").val();
    
            if(state=="drink")console.log(`${searchDrink} ${describeDrink} ${rateDrink}`);
            else if(state=="bar")console.log(`${searchBar} ${rateBar}`);
    
    * Issue with .php file, pushing all other files to the branch
    
    *   Modified backend/rateDrinknBar.php	Please enter the commit message for your changes. Lines starting
    
    * I'm saving a successful logout function in Varinder's page. HOwever, rating submission still needs work.
    
    * Update rating for drink and bar is working

---
+++ backend/rateDrinknBar.php
@@ -0,0 +1,160 @@
+<?php
+    //  PROJECT:        BeerBuddy
+    //  Name:           Varinder Singh
+    //  File:           rateDrinknBar.php
+    //  Date Created:   April 30, 2018
+    //  Last Modified:  April 30, 2018
+    //  Details:
+    //                  This file handles all front end POST requests that read rating data from the database.
+    //  Dependencies:
+    //                  PHP
+    //                  MySQL
+    //                  Apache Web Server
+
+    require  "utility.php";
+    require  "required/credentials.php";
+
+            // Acquire POST request body parameter
+
+    $response = formatResponse("success", $data);
+
+    $credentials = $_CREDENTIALS["db"];
+
+
+
+    // I'll disallow GET requests to this file for now
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        if ($_GET['action'] === "getpwd") {
+            $plaintextPassword = $_GET['pwd'];
+            $response = formatResponse("success", hash("sha256", $plaintextPassword));
+        } else {
+            $response = formatResponse("failure", "Method GET not permitted");
+        }
+    } else {
+        processInput();
+    }
+
+    replyToClient($response);
+
+
+ // @function    processInput
+    // @parameter   n/a
+    // @returns     n/a
+    // @details     This function processes input from the client and responds based on the information given
+    function processInput() {
+        global $state, $response, $data, $credentials;  // force PHP to use the global variables
+        $state = $_POST['state'];
+        $username = $_POST['username'];
+        $data["searchBar"] = $_POST['btitle'];
+        $rateBar = intval($_POST['brating']);
+        $searchDrink = $_POST['dtitle'];
+        $data["searchBarForDrink"] = $_POST['btitleford'];
+        $describeDrink = $_POST['ddescription'];
+        $rateDrink = intval($_POST['drating']);
+
+        // Instantiate a MySQL Connection
+        $mysqli = new mysqli($credentials["host"], $credentials["user"], $credentials["pwd"], $credentials["name"]);
+        if ($mysqli->connect_errno) {
+            $response = formatResponse("failure", "MySQL connection failed: ($mysqli->connect_errno) $mysqli->connect_error");
+        } else {
+            // Perform requested action
+            try {
+                switch ($state) {
+                    case "bar":
+
+                        //find bar_id if it excists $averages->fetch_assoc()
+                        $excistingBar = $mysqli->query("SELECT bar_id FROM bar WHERE bar_name LIKE '%$searchBar%';");   
+                        
+                        if(mysqli_num_rows($excistingBar)>0){
+
+                            $rowOfTable = $excistingBar->fetch_assoc();
+                            $barID = $rowOfTable['bar_id'];
+
+                            $response = formatResponse("failure", $_POST['temp']);
+
+                            // Update the bars rating
+                            // $newBRating = $mysqli->query("UPDATE rate_bar SET stars = 4 WHERE bar_id = 1 AND username='vsingh95';");
+                            $newBRating = $mysqli->query("UPDATE rate_bar SET stars = $rateBar WHERE bar_id = $barID AND username = '$username';");
+                            if ($newBRating === FALSE) {
+                                // $emsg = $barID;
+                                $emsg = mysqli_error($mysqli);
+                                $equery = "UPDATE rate_bar SET stars = $rateBar WHERE bar_id = $barID AND username = '$username';";
+                                $response = formatResponse("failure", "Failed to update database: $emsg \n $equery");
+                            } else {
+                                $response = formatResponse("success", "Update Successful");
+                            }
+
+                            // if($newBRating) $response = formatResponse("success", "Successful Update!");
+                            // else{
+                            //     $emsg = mysqli_error($mysqli);
+                            //     $response = formatResponse("failure", "Update Failed: $emsg");
+                            // }
+
+
+                        }else if(mysqli_num_rows($excistingBar)==0){
+                            //bar name doesn't exist
+                            $response = formatResponse("failure", "Bar is incorrect or doesn't exist!");
+
+                        }else{
+                            //error
+                            $response = formatResponse("failure", "Unexpected Error!");
+
+                        }
+                        break;
+
+                    case "drink":
+                        
+                        //find bar_id if it excists
+                        $excistingBarDrink = $mysqli->query("SELECT bar_id FROM bar WHERE bar_name LIKE '%$searchBarForDrink%'");   
+
+
+
+                        //find drink_id if it excists
+                        $excistingDrink = $mysqli->query("SELECT name FROM drink WHERE  name LIKE '%$searchDrink%'");   
+
+
+                        //Drink excists and bar excists
+                        if(mysqli_num_rows($excistingBarDrink)>0 && mysqli_num_rows($excistingDrink)>0){
+
+                            $rowOfTable = $excistingBarDrink->fetch_assoc();
+                            $excistingBarDrink = $rowOfTable['bar_id'];
+
+                            $rowOfTable = $excistingDrink->fetch_assoc();
+                            $excistingDrink = $rowOfTable['name'];
+
+                            $newDRating= $mysqli->query("UPDATE rate_drink SET stars = $rateDrink WHERE bar_id = $excistingBarDrink AND username = '$username' AND name = '$excistingDrink'");
+
+                            $newDDescription= $mysqli->query("UPDATE rate_drink SET taste = '$describeDrink' WHERE bar_id = $excistingBarDrink AND username = '$username' AND name = '$excistingDrink'");
+
+                            if($newDRating) $response = formatResponse("success", "Successful Rating!");
+                            else $response = formatResponse("failure", "Update Failed!");
+
+                            if($newDDescription) $response = formatResponse("success", "Successful Description!");
+                            else $response = formatResponse("failure", "Update Failed!");
+
+                        } else {
+
+                        }
+
+                        break;
+
+                    default:
+                        //$response = formatResponse("success", "Bar!");
+
+                        $response = formatResponse("failure","Invalid action '$action'");
+                        break;
+                }
+            } catch (Exception $e) {
+                $response = formatResponse("failure", "An error occurred: $e");
+            }
+        }
+
+        // Close instance
+        mysqli_close($mysqli);
+        return;
+    }
+
+
+    // END readRating.php
+
+?>
---
+++ js/rateDrinknBar.js
@@ -0,0 +1,210 @@
+//
+//  PROJECT:        BeerBuddy
+//  Name:           Varinder Singh
+//  File:           ratingsPage.js
+//  Date Created:   April 30, 2018
+//  Last Modified:  April 30, 2018
+//  Details:
+//                  This file contains javascript code that runs the ratingsPage.html UI
+//  Dependencies:
+//                  JS ECMAscript 6
+//                  utility.js
+
+// Model
+var model = {
+    "searchType": "drink"
+};
+
+$(document).ready(function () {
+    // Start page with initial values
+    init();
+
+    $('head').append('<style>.outlineElement{background-color:Yellow;} </style>');          
+
+    $(".highlightable")
+    .mouseover(function(event) {
+    $(event.target).addClass('outlineElement');
+    })
+    .mouseout(function(event) {
+    $(event.target).removeClass('outlineElement');
+    });    
+
+    //adding state bar to intialization 
+    var state= "bar"; 
+
+    //hide the bar inputs
+    $("#drink_inputs").addClass("hidden");
+    //show the drink inputs
+    $("#bar_inputs").removeClass("hidden");
+
+    //
+
+    //Bind submission to submit button
+    $("#submitInfo").on("click", function(){
+        var searchDrink = $("#searchDrink").val();
+        var searchBarForDrink = $("#searchBarForDrink").val();
+        var describeDrink = $("#describeDrink").val();
+        var rateDrink = $("#rateDrink").val();
+        var searchBar = $("#searchBar").val();
+        var rateBar = $("#rateBar").val();
+
+
+        if(state=="drink")console.log(`${searchDrink} ${searchBarForDrink} ${describeDrink} ${rateDrink}`);
+        else if(state=="bar")console.log(`${searchBar} ${rateBar}`);
+
+        //function submitRating (searchBar, rateBar, searchDrink, describeDrink, rateDrink, state) {
+        submitRating(searchBar, rateBar, searchDrink, searchBarForDrink, describeDrink, rateDrink, state);
+
+
+    });
+
+
+    // Bind mode changing to the click of the mode change button
+    $('#changeMode').on("click", function(){
+        if(state=="bar"){
+            state="drink";
+            //hide the bar inputs
+            $("#bar_inputs").addClass("hidden");
+            //show the drink inputs
+            $("#drink_inputs").removeClass("hidden");
+
+        }
+        else if(state=="drink"){
+            state="bar";  
+            //hide drink inputs
+            $("#drink_inputs").addClass("hidden");
+            //show bar inputs 
+            $("#bar_inputs").removeClass("hidden");
+
+        }
+
+        console.log(`${state}`);
+    });
+
+    // Bind logout to the logout button
+    $("#logout").on("click", function () {
+        var data = {
+            // nothing
+        };
+        var callback = function (reply, status, jqxhr) {
+            try {
+                reply = JSON.parse(reply);
+                switch (status) {
+                    case "success": {
+                        var redirect = reply.message.redirect;
+                        window.location = redirect;
+                        break;
+                    }
+                    default: {
+                        console.log("Yo. fix me.");
+                        break;
+                    }
+                }
+            } catch (e) {
+                console.log("Darnit");
+            }
+        };
+        var url = "http://localhost/beerbuddy/backend/logout.php";
+
+        post(url, data, callback, true);
+    });
+});
+
+/*function init() {
+    $("#searchType").html(model.searchType);
+}*/
+
+function init() {
+    $("#searchType").html(model.searchType);
+    getCurrentUser();
+}
+
+// Controllers
+/*
+    @function   setSearchType
+    @parameter  str - the value to set the search type as
+    @returns    n/a
+    @details    This function sets the search type by updating the model value and setting the UI to the new search type
+*/
+function setSearchType (str) {
+    updateModel("searchType", str);
+    $("#searchType").html(model.searchType);
+}
+
+/*
+    @function   updateModel
+    @parameter  key - the name of the key to update
+    @parameter  value - the value to update with
+    @returns    n/a
+    @details    This function updates the model with new data, or creates a new value if it doesn't exist
+*/
+function updateModel (key, value) {
+    model[key] = value;
+}
+
+/*
+    @function   submitRating
+    @parameter  searchBar - submit current search request for a bar title 
+    @parameter  rateBar - submit a numerical value assosiated with a bar (1-5 stars)  
+    @parameter  searchDrink - submit current search request for a drink title
+    @parameter  describeDrink - submit drink description
+    @parameter  rateDrink - sumbit a numerical value assoiated with a drink (1-5 stars)
+    @parameter  state - switches between bar and drink 
+    @returns    n/a
+    @details    This function submits a drink or bar search
+*/
+function submitRating (searchBar, rateBar, searchDrink, searchBarForDrink, describeDrink, rateDrink, state) {
+    var username = $("#username").html();
+
+    var data = {
+        "btitle": searchBar,
+        "brating": Number.parseInt(rateBar),
+        "dtitle": searchDrink,
+        "btitleford": searchBarForDrink,
+        "ddescription": describeDrink,
+        "drating": Number.parseInt(rateDrink),
+        "state": state,
+        "username": username
+    };
+
+    var callback = function (reply, status, jqxhr) {
+        console.log(`Status: ${reply}`);  // debug
+    };
+
+    //reply will be echo
+    post("http://localhost/beerbuddy/backend/rateDrinknBar.php",data,callback, true); 
+
+}
+
+// @function    getCurrentUser
+// @returns     n/a
+// @details     This function acquires the session's associated username and populates any relevant UI elements with this information.
+function getCurrentUser () {
+    var data = {
+        "action": "getInfo",
+        "data": ""
+    };
+    var url = "http://localhost/beerbuddy/backend/readRating.php";
+    var callback = function (reply, status, jqxhr) {
+        console.log(reply);
+        try {
+            reply = JSON.parse(reply);
+            switch (status) {
+                case "success": {
+                    var currentUser = reply.message.username;
+                    $("#username").html(currentUser);
+                    break;
+                }
+                default: {
+                    console.log("Please fix this issue");
+                    break;
+                }
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    post(url, data, callback, true);
+}
+// END ratingsPage.js
